679b3f3250a18f41e03cd24f0fba4b471d981225
pablolizardo <pablo_lizardo@yahoo.com.ar>
2014-07-31 02:13:23 -0300

    actualizado el css y agregado el sprite

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -25,20 +25,20 @@ $(document).ready(function() {
            // $('body').css('-webkit-filter', 'invert(1)');
         });
 
-   
+
     // svg part
-    // Now lets load external SVG file:
-    var s = Snap("#bulb");
-    Snap.load("../images/svg/bulb.svg", function (f) {
-        // Note that we traversre and change attr before SVG
-        // is even added to the page
-        //f.selectAll("polygon[fill='#09B39C']").attr({fill: "#bada55"});
-        //g = f.select("g");
-        s.append(f);
-        // Making croc draggable. Go ahead drag it around!
-        //s.drag();
-        // Obviously drag could take event handlers too
-        // That’s better! selectAll for the rescue.
-    });
+    // // Now lets load external SVG file:
+    // var s = Snap("#bulb");
+    // Snap.load("../images/svg/bulb.svg", function (f) {
+    //     // Note that we traversre and change attr before SVG
+    //     // is even added to the page
+    //     //f.selectAll("polygon[fill='#09B39C']").attr({fill: "#bada55"});
+    //     //g = f.select("g");
+    //     s.append(f);
+    //     // Making croc draggable. Go ahead drag it around!
+    //     //s.drag();
+    //     // Obviously drag could take event handlers too
+    //     // That’s better! selectAll for the rescue.
+    // });
 
 });
